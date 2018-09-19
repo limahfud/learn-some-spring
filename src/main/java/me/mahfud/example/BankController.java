31d9e91500720dafb871a989e3bd2a9c5715a56c
Ali Mahfud <alimahfudpr@gmail.com>
2018-09-19 12:08:41 +0700

    [ALI] add CRUD to table banks

--- src/main/java/me/mahfud/example/BankController.java
+++ src/main/java/me/mahfud/example/BankController.java
@@ -1,5 +1,6 @@
 package me.mahfud.example;
 
+import com.google.common.base.Preconditions;
 import me.mahfud.example.model.Bank;
 import me.mahfud.example.repositories.BankRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BankController {
@@ -26,9 +28,9 @@ public class BankController {
         List<Bank> banks = new ArrayList<>();
 
         HashMap<String, Object> groups = new HashMap<>();
-        banks.add(new Bank(1, "Bank Permata"));
-        banks.add(new Bank(2, "Bank BPD DIY"));
-        banks.add(new Bank(3, "Bank Mega"));
+        banks.add(new Bank(1, "Bank Permata", "012010212"));
+        banks.add(new Bank(2, "Bank BPD DIY", "0121212"));
+        banks.add(new Bank(3, "Bank Mega", "208431924731"));
         groups.put("banks", banks);
         groups.put("min", Integer.parseInt(minimum));
         groups.put("error", false);
@@ -37,10 +39,38 @@ public class BankController {
     }
 
     @PostMapping(value = "/app")
-    public String postBank(@RequestParam(name = "min", defaultValue = "10") String minimum,
-                           @RequestBody Bank string) {
+    public Bank postBank(@RequestParam(name = "min", defaultValue = "10") String minimum,
+                           @RequestBody Bank bank) {
+        Preconditions.checkNotNull(bank);
+        return bankRepository.save(bank);
+    }
 
-        System.out.println("Parameter : " + string.toString());
-        return "success";
+    @PutMapping(value = "/bank/{id}/update")
+    public Bank putBank(@PathVariable(name = "id") String bankId, @RequestBody Bank bank) {
+        Integer id = Integer.parseInt(bankId);
+
+        Preconditions.checkNotNull(bank);
+
+        Bank oldBank = bankRepository.getOne(id);
+        oldBank.setBank(bank.getBank());
+
+        System.out.println("Update the bank with " + oldBank.toString());
+
+        return bankRepository.save(oldBank);
+    }
+
+    @DeleteMapping(value = "/bank/{id}/delete")
+    public HashMap<String, Boolean> deleteBank(@PathVariable(name = "id") String bankId) {
+        Optional<Bank> bankQuery = bankRepository.findById(Integer.parseInt(bankId));
+
+        if (bankQuery.isPresent()) {
+            Bank bank = bankQuery.get();
+            bankRepository.delete(bank);
+        }
+
+        HashMap<String, Boolean> response = new HashMap<>();
+        response.put("success", bankQuery.isPresent());
+        
+        return response;
     }
 }
--- src/main/java/me/mahfud/example/model/Bank.java
+++ src/main/java/me/mahfud/example/model/Bank.java
@@ -2,21 +2,28 @@ package me.mahfud.example.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 
 @Entity
 public class Bank {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Size(min = 3, max = 100)
     private String bank;
+
+    @Size(min = 3, max = 100)
+    @Column(name = "atas_nama")
+    private String name;
+
+    @Column(name = "no_rekening")
+    private String number;
+
+    @Column(name = "logo")
+    private String logo;
 
     public Bank() {}
 
@@ -24,9 +31,10 @@ public class Bank {
         this.bank = bank;
     }
 
-    public Bank(int id, String bank) {
+    public Bank(int id, String bank, String name) {
         this.id = id;
         this.bank = bank;
+        this.name = name;
     }
 
     @JsonProperty("id")
@@ -39,12 +47,10 @@ public class Bank {
         this.id = id;
     }
 
-    @JsonProperty("bank_name")
     public String getBank() {
         return bank;
     }
 
-    @JsonProperty("bank_name")
     public void setBank(String bank) {
         this.bank = bank;
     }
@@ -54,6 +60,32 @@ public class Bank {
         return "Bank{" +
                 "id=" + id +
                 ", bank='" + bank + '\'' +
+                ", name='" + name + '\'' +
                 '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
     }
 }
