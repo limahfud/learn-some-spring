17e25b6b332680db4886da3d77e2435bb9dda0ab
Ali Mahfud <alimahfudpr@gmail.com>
2018-10-08 10:48:01 +0700

    [ALI] implement unit test in BankTest.java

--- src/main/java/me/mahfud/example/controller/BankController.java
+++ src/main/java/me/mahfud/example/controller/BankController.java
@@ -4,6 +4,7 @@ import com.google.common.base.Preconditions;
 import me.mahfud.example.api.model.Response;
 import me.mahfud.example.model.Bank;
 import me.mahfud.example.repositories.BankRepository;
+import me.mahfud.example.request.BankRequestVo;
 import me.mahfud.example.response.vo.BankItem;
 import me.mahfud.example.response.vo.DetailBankVo;
 import me.mahfud.example.service.BankService;
@@ -12,9 +13,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.xml.soap.Detail;
 import java.util.*;
 
 @RestController
+@CrossOrigin
 public class BankController {
 
     @Autowired
@@ -41,77 +44,35 @@ public class BankController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PostMapping("/bank")
-    public ResponseEntity<Response> detail(@RequestBody Bank bank) {
+    @RequestMapping(value = "/bank", method = RequestMethod.POST)
+    public ResponseEntity<Response> createBank(@RequestBody BankRequestVo bankRequestVo) {
 
-        return null;
+        DetailBankVo detail = bankService.createBank(bankRequestVo);
+
+        Response response = new Response(HttpStatus.OK, detail);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @GetMapping(value = "/app")
-    public ResponseEntity<HashMap<String, Object>> getBank(@RequestParam(name = "min", defaultValue = "10") String minimum) {
-        List<Bank> banks = new ArrayList<>();
+    @PutMapping(value = "/bank/{id}")
+    public ResponseEntity<Response> updateBank(@RequestBody BankRequestVo bankRequestVo,
+                                               @PathVariable("id") Long id) {
 
-        HashMap<String, Object> groups = new HashMap<>();
-        banks.add(new Bank(1, "Bank Permata", "012010212"));
-        banks.add(new Bank(2, "Bank BPD DIY", "0121212"));
-        banks.add(new Bank(3, "Bank Mega", "208431924731"));
-        groups.put("banks", banks);
-        groups.put("min", Integer.parseInt(minimum));
-        groups.put("error", false);
+        DetailBankVo detail = bankService.updateBank(bankRequestVo, id);
 
-        return new ResponseEntity<>(groups, HttpStatus.CREATED);
+        Response response = new Response(HttpStatus.OK, detail);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PostMapping(value = "/app")
-    public Bank postBank(@RequestParam(name = "min", defaultValue = "10") String minimum,
-                           @RequestBody Bank bank) {
-        Preconditions.checkNotNull(bank);
-        return bankRepository.save(bank);
+    @DeleteMapping("/bank/{id}")
+    public ResponseEntity<Response> deleteBank(@PathVariable("id") Long id) {
+        DetailBankVo detail = bankService.deleteBank(id);
+
+        Response response = new Response(HttpStatus.OK, detail);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    @PutMapping(value = "/bank/{id}/update")
-    public Bank putBank(@PathVariable(name = "id") String bankId, @RequestBody Bank bank) {
-        Integer id = Integer.parseInt(bankId);
 
-        Preconditions.checkNotNull(bank);
-
-        Bank oldBank = bankRepository.getOne(id.longValue());
-        oldBank.setBank(bank.getBank());
-
-        System.out.println("Update the bank with " + oldBank.toString());
-
-        return bankRepository.save(oldBank);
-    }
-
-    @DeleteMapping(value = "/bank/{id}/delete")
-    public ResponseEntity<HashMap<String, Boolean>> deleteBank(@PathVariable(name = "id") String bankId) {
-        Optional<Bank> bankQuery = bankRepository.findById(Long.parseLong(bankId));
-
-        if (bankQuery.isPresent()) {
-            Bank bank = bankQuery.get();
-            bankRepository.delete(bank);
-        }
-
-        HashMap<String, Boolean> response = new HashMap<>();
-        response.put("success", bankQuery.isPresent());
-
-        return new ResponseEntity<HashMap<String, Boolean>>(response, HttpStatus.OK);
-    }
-
-    private HashMap<String, Object> getResponse(Object object, String dataKey) {
-
-        HashMap<String, Object> response = new HashMap<>();
-
-        HashMap<String, Object> data = new HashMap<>();
-        data.put(dataKey, object);
-        response.put("status", true);
-        response.put("data", data);
-        response.put("message", "success");
-
-        return response;
-    }
-
-    private HashMap<String, Object> getResponse(Object object) {
-        return getResponse(object, "data");
-    }
 }
--- src/main/java/me/mahfud/example/service/BankService.java
+++ src/main/java/me/mahfud/example/service/BankService.java
@@ -5,6 +5,7 @@ import me.mahfud.example.model.Bank;
 import me.mahfud.example.model.BankUser;
 import me.mahfud.example.repositories.BankRepository;
 import me.mahfud.example.repositories.BankUserRepository;
+import me.mahfud.example.request.BankRequestVo;
 import me.mahfud.example.response.converter.BankDetailMapper;
 import me.mahfud.example.response.mapper.BankUserToUserMapper;
 import me.mahfud.example.response.vo.*;
@@ -17,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.ModelMap;
 
+import javax.xml.soap.Detail;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
@@ -51,13 +53,64 @@ public class BankService {
         return items;
     }
 
+    public List<BankItem> searchBankByName(String query) {
+        List<Bank> banks = bankRepository.findByNameContaining(query);
+        
+        List<BankItem> bankItems = new ArrayList<>();
+        banks.forEach(bank -> {
+            bankItems.add(new BankItem(bank.getId(), bank.getName()));
+        });
+
+        return bankItems;
+    }
+    
     public DetailBankVo bankItemDetail(Long id) {
         if (bankRepository.findById(id).isPresent()) {
             Bank bank = bankRepository.findById(id).get();
 
             return mapper.map(bank, DetailBankVo.class);
         } else {
-            throw new BusNotFoundException("the bus is exception", 1L);
+            throw new BusNotFoundException("The resource is not found", 1L);
         }
+    }
+
+    public DetailBankVo createBank(BankRequestVo bankRequestVo) {
+        Bank newBank = new Bank();
+        newBank.setName(bankRequestVo.getName());
+        newBank.setBank(bankRequestVo.getBank());
+        newBank.setLogo(bankRequestVo.getLogo());
+        newBank.setNumber(bankRequestVo.getNumber());
+        newBank.setBankUserList(new ArrayList());
+
+        Bank createdBank = bankRepository.save(newBank);
+        return mapper.map(createdBank, DetailBankVo.class);
+    }
+
+    public DetailBankVo updateBank(BankRequestVo bankRequestVo, Long id) {
+        if (!bankRepository.findById(id).isPresent()) {
+            throw new BusNotFoundException("The resource is not found", id);
+        }
+        Bank updatedBank = bankRepository.findById(id).get();
+
+        updatedBank.setName(bankRequestVo.getName());
+        updatedBank.setBank(bankRequestVo.getBank());
+        updatedBank.setLogo(bankRequestVo.getLogo());
+        updatedBank.setNumber(bankRequestVo.getNumber());
+
+        Bank savedBank = bankRepository.save(updatedBank);
+
+        return mapper.map(savedBank, DetailBankVo.class);
+    }
+
+    public DetailBankVo deleteBank(Long id) {
+        if (!bankRepository.findById(id).isPresent()) {
+            throw new BusNotFoundException("The resource is not found", id);
+        }
+
+        Bank deletedBank = bankRepository.findById(id).get();
+
+        bankRepository.delete(deletedBank);
+
+        return mapper.map(deletedBank, DetailBankVo.class);
     }
 }
